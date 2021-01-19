5ea03827d6c78df4d1014930df68cedd6d23a7d7
brahim024 <ifninos168@gmail.com>
2021-01-19 16:17:42 +0100

    add cbv to get data

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-def index(request):
-    return HttpResponse('its work')
+from django.views.generic import TemplateView
+from .models import Club
+
+
+
+class ClubChartView(TemplateView):
+	templates_name='clubs/chart.js'
+	def get_context_data(self,**kwargs):
+		context=super().get_context_data(**kwargs)
+		context['qs']=Club.objects.all()
+		return context
